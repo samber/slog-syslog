49c4d9cb7c804959005f3857e69dfb5d397f9f0d
Vanik Hachatryan <vanixachatryan@outlook.com>
2024-12-09 10:07:44 +0300

    tests: back `goleak`, delete debug test

--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/go-playground/assert/v2 v2.2.0
 	github.com/google/uuid v1.6.0
 	github.com/samber/lo v1.44.0 // indirect
+	go.uber.org/goleak v1.3.0
 	golang.org/x/text v0.16.0 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -1,40 +1,12 @@
 package slogsyslog
 
 import (
-	"encoding/json"
-	"log/slog"
-	"net"
 	"testing"
 
-	"github.com/go-playground/assert/v2"
+	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
-	m.Run()
+	goleak.VerifyTestMain(m)
 
-}
-
-func TestForDebugLog(t *testing.T) {
-	handler, err := getSlogSyslogHandler()
-
-	assert.Equal(t, err, nil)
-
-	logger := slog.New(handler)
-	slog.SetDefault(logger)
-
-	slog.Info("test rsys log", slog.String("key1", "val1"))
-}
-
-func getSlogSyslogHandler() (slog.Handler, error) {
-	writer, err := net.Dial("udp", ":514")
-	if err != nil {
-		return nil, err
-	}
-
-	return Option{
-		Converter: DefaultConverter,
-		Level:     slog.LevelDebug,
-		Writer:    writer,
-		Marshaler: json.Marshal,
-	}.NewSyslogHandler(), nil
 }
